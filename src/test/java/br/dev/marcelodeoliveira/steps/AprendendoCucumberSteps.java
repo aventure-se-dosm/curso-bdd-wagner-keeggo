d82da37fd60a44f44eaa07161e9f0f56b00b35b2
Marcelo de Oliveira Santos <>
2023-01-23 19:15:07 -0300

    aula 12 finalizada: [1] cenário de atraso com atraso suportando 'dia', 'mês' e ano', plural e pt/en; [2] remoção de arquivos já enviados de .class, force do .gitignore para prevenção de arquivos indesejados no repositório remoto

--- src/test/java/br/dev/marcelodeoliveira/steps/AprendendoCucumberSteps.java
+++ src/test/java/br/dev/marcelodeoliveira/steps/AprendendoCucumberSteps.java
@@ -1,7 +1,10 @@
 package br.dev.marcelodeoliveira.steps;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.Period;
+
+import org.junit.Assert;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -11,11 +14,12 @@ import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
 
 public class AprendendoCucumberSteps {
-	
-	private int contador = 15;
-	
-	
-	
+
+	private Integer contador = 15;
+
+//	private Date entrega = new Date();
+	private LocalDate entrega = null;
+
 	@Given("que criei o arquivo")
 	public void que_criei_o_arquivo() {
 		System.out.println("Arquivo criado com sucesso!");
@@ -55,35 +59,100 @@ public class AprendendoCucumberSteps {
 		System.out.println("Execution finished successfully.");
 
 	}
-	
+
 	@Dado("que o valor do contador é {int}")
 	public void queOValorDoContadorÉ(Integer int1) {
-	    
-	contador = int1;
-		
+
+		contador = int1;
+
 		// Write code here that turns the phrase above into concrete actions
-	   // throw new io.cucumber.java.PendingException();
+		// throw new io.cucumber.java.PendingException();
 	}
+
 	@Quando("eu incrementá-lo em {int}")
 	public void euIncrementáLoEm(Integer int1) {
-		
+
 		contador += int1;
-	    // Write code here that turns the phrase above into concrete actions
-	   // throw new io.cucumber.java.PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new io.cucumber.java.PendingException();
 	}
+
 	@Então("o seu valor será {int}")
 	public void oSeuValorSerá(Integer int1) {
-		
-		//fazer o assert falhar: int1 = 45;
+
+		// fazer o assert falhar: int1 = 45;
 		System.out.printf("\n%s\t%d\n%s\t%d\n\n", "Valor Esperado:", contador, "Valor obtido:", int1);
-		
+
 		// lançar uma exceção pra causar erro:
 		// visto como falha:
 		// throw new RuntimeException();
-		assertEquals(contador, int1);
-		
-	    // Write code here that turns the phrase above into concrete actions
-	    //throw new io.cucumber.java.PendingException();
+
+//		Calendar calendar = Calendar.getInstance();
+//		calendar.set(2018, 4, 5, 0, 0, 0);
+		// entrega = calendar.getTime();
+
+		Assert.assertEquals(contador, int1);
+
 	}
-	
+
+	@Dado("que o prazo é dia {int}\\/{int}\\/{int}")
+	public void queOPrazoÉDia(Integer dia, Integer mes, Integer ano) {
+
+		entrega = LocalDate.of(2018, 4, 5);
+		System.out.println(entrega.toString());
+		System.out.println("/n/n" + "Data de entrega inicial: " + entrega.toString() + "/n/n");
+
+	}
+
+	@Quando("a entrega atrasar em {int} {string}")
+	public void aEntregaAtrasarEm(int quantidade, String unidadeSemantica) {
+
+		Period tempUnit = null;
+
+		switch (unidadeSemantica.toLowerCase()) {
+		case "dia":
+		case "dias":
+		case "days": {
+			tempUnit = Period.ofDays(quantidade);
+			break;
+		}
+		case "mes":
+		case "mês":
+		case "meses":
+		case "month":
+		case "months": {
+			tempUnit = Period.ofMonths(quantidade);
+			break;
+		}
+
+		case "ano":
+		case "anos":
+		case "year":
+		case "years": {
+			tempUnit = Period.ofYears(quantidade);
+			break;
+		}
+
+		}
+
+		try {
+
+			entrega = entrega.plus(tempUnit);
+			System.out.println(entrega.toString());
+		} catch (DateTimeException | ArithmeticException a) {
+				System.out.println(a.getLocalizedMessage());;
+		}
+	}
+
+	// entrega = entrega.plusDays(dias);
+	// unidade.
+	// TemporalAmount: classe de unidade te tempo
+
+	@Então("a entrega será efetuada em {int}\\/{int}\\/{int}")
+	public void aEntregaSeráEfetuadaEm(Integer dia, Integer mes, Integer ano) {
+
+		System.out.println("\n\n" + "Data de entrega final: " + entrega.toString() + "\n\n");
+		Assert.assertEquals(entrega, LocalDate.of(ano, mes, dia));
+	} 
+
 }
--- src/test/resources/aprendendo_cucumber.feature
+++ src/test/resources/aprendendo_cucumber.feature
@@ -12,3 +12,15 @@ Para que eu possa automatizar os critérios de aceitação
   	Dado que o valor do contador é 15
   	Quando eu incrementá-lo em 3
   	Então o seu valor será 18  
+  	
+  Cenário: Deve incrementar contador
+  	Dado que o valor do contador é 123
+  	Quando eu incrementá-lo em 35
+  	Então o seu valor será 158
+  	
+  	
+ 	Cenário: Deve calcular atraso no prazo de entrega
+ 	
+ 	Dado que o prazo é dia 05/04/2018
+ 	Quando a entrega atrasar em 2 'dias'
+ 	Então a entrega será efetuada em 07/04/2018
